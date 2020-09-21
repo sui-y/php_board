3cb8cd8768fd714b120fddcc5c0118d01b04b0c5
sui <43640408+sui-y@users.noreply.github.com>
2020-09-21 21:19:45 +0800

    fixed connMysql

--- connMysql.php
+++ connMysql.php
@@ -1,5 +1,9 @@
 <?php
-  $db_link=@new mysqli("192.168.2.200", "suistude_board", "b123yz36", "suistude_board");
+  $db_host = "192.168.2.200";
+  $db_username = "suistude_board";
+  $db_password = "b123yz36";
+  $db_name = "suistude_board";
+  $db_link=@new mysqli($db_host, $db_username, $db_password, $db_name);
   if($db_link->connect_error != ""){
     echo "資料庫連結失敗";
   }else{
